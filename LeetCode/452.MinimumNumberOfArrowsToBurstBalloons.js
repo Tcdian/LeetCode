d8b0ca67199bef298baa1ea099e84d25635ad23b
Tcdian <wenkai>
2018-08-19 22:40:08 +0800

    452. Minimum Number of Arrows to Burst Balloons

--- LeetCode/452.MinimumNumberOfArrowsToBurstBalloons.js
+++ LeetCode/452.MinimumNumberOfArrowsToBurstBalloons.js
@@ -4,6 +4,7 @@
  */
 
 // 将 points 按 points[0] 按从小到大排序, 再保证可以射击到前面气球的情况下, 尽可能多的射击到更多的气球, 当无法射击到更多气球时, 增加新的弓箭手
+// 左端点因为已经排好序, 后面的一定比前面的大, 因此后面每次的 points[i][0]即为左端点, 不需要去记录
 var findMinArrowShots = function (points) {
   if (points.length === 0) return 0
   points.sort((a, b) => a[0] - b[0])
