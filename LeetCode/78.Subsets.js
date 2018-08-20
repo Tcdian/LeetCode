1d8c2300f692599cbe1836bac90a3a543337d6ed
Tcdian <wenkai>
2018-08-20 10:53:26 +0800

    78. Subsets

--- LeetCode/78.Subsets.js
+++ LeetCode/78.Subsets.js
@@ -4,19 +4,20 @@
  */
 
 //对于数组中的每个元素, 都有选或者不选两种情况, 回溯处理每种情况
-var subsets = function(nums) {
-    let result = [[]]
-    let item = []
-    function backtracking(i=0) {
-        if (i >= nums.length) {
-            return
-        }
-        item.push(nums[i])
-        result.push(item.slice())
-        backtracking(i + 1)
-        item.pop()
-        backtracking(i + 1)
-        return result
+var subsets = function (nums) {
+  let result = [[]]
+  let item = []
+
+  function backtracking(i = 0) {
+    if (i >= nums.length) {
+      return
     }
-    return backtracking() || result
+    item.push(nums[i])
+    result.push(item.slice())
+    backtracking(i + 1)
+    item.pop()
+    backtracking(i + 1)
+    return result
+  }
+  return backtracking() || result
 }
