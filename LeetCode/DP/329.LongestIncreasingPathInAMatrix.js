5060e59f28dd0d4606618846c11b509bbb7e9f84
Tcdian <wenkai>
2018-08-23 16:02:05 +0800

    329. Longest Increasing Path in a Matrix

--- LeetCode/DP/329.LongestIncreasingPathInAMatrix.js
+++ LeetCode/DP/329.LongestIncreasingPathInAMatrix.js
@@ -25,24 +25,21 @@ var longestIncreasingPath = function (matrix) {
   })
   let max = 1
   let dp = new Array(matrix.length).fill(0).map(it => new Array(matrix[0].length).fill(1))
+  let directionX = [-1, 0, 1, 0]
+  let directionY = [0, 1, 0, -1]
   for (let k = 0; k < incrementSortPosition.length; k++) {
     let i = incrementSortPosition[k][0]
     let j = incrementSortPosition[k][1]
-    if (i > 0 && matrix[i][j] < matrix[i - 1][j]) {
-      dp[i - 1][j] = Math.max(dp[i - 1][j], dp[i][j] + 1)
-      max = Math.max(max, dp[i - 1][j])
-    }
-    if (j < matrix[i].length - 1 && matrix[i][j] < matrix[i][j + 1]) {
-      dp[i][j + 1] = Math.max(dp[i][j + 1], dp[i][j] + 1)
-      max = Math.max(max, dp[i][j + 1])
-    }
-    if (i < matrix.length - 1 && matrix[i][j] < matrix[i + 1][j]) {
-      dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j] + 1)
-      max = Math.max(max, dp[i + 1][j])
-    }
-    if (j > 0 && matrix[i][j] < matrix[i][j - 1]) {
-      dp[i][j - 1] = Math.max(dp[i][j - 1], dp[i][j] + 1)
-      max = Math.max(max, dp[i][j - 1])
+    for (let d = 0; d < 4; d++) {
+      let dx = i + directionX[d]
+      let dy = j + directionY[d]
+      if (dx < 0 || dx >= matrix.length || dy < 0 || dy >= matrix[i].length) {
+        continue
+      }
+      if (matrix[i][j] < matrix[dx][dy]) {
+        dp[dx][dy] = Math.max(dp[dx][dy], dp[i][j] + 1)
+        max = Math.max(max, dp[dx][dy])
+      }
     }
   }
   return max
