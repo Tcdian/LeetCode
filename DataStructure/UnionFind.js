d27d79297c710ee385d0a9fd4718f38bb7936df4
Tcdian <wenkai>
2018-08-26 11:38:29 +0800

    UnionFind

--- DataStructure/UnionFind.js
+++ DataStructure/UnionFind.js
@@ -4,19 +4,11 @@ class UnionFind {
     this.rank = new Array(size).fill(1)
     this.parent = new Array(size).fill(1).map((it, index) => index)
   }
+
   getSize() {
     return this._size
   }
-  _find(p) {
-    if (p >= this.getSize() || p < 0) {
-      throw new Error('out of bound')
-    }
-    while(this.parent[p] !== p) {
-      this.parent[p] = this.parent[this.parent[p]]
-      p = this.parent[p]
-    }
-    return p
-  }
+
   union(p, q) {
     let pRoot = this._find(p)
     let qRoot = this._find(q)
@@ -29,9 +21,21 @@ class UnionFind {
       this.rank[qRoot]++
     }
   }
+
   isConnected(p, q) {
     let pRoot = this._find(p)
     let qRoot = this._find(q)
     return pRoot === qRoot
+  }
+
+  _find(p) {
+    if (p >= this.getSize() || p < 0) {
+      throw new Error('out of bound')
+    }
+    while (this.parent[p] !== p) {
+      this.parent[p] = this.parent[this.parent[p]]
+      p = this.parent[p]
+    }
+    return p
   }
 }
