eac49c4d2c222992ecb0436f8a59f71c1241a682
Tcdian <wenkai>
2018-08-27 22:33:13 +0800

    315. Count of Smaller Numbers After Self

--- LeetCode/BinarySearch&DivideAndConquer/315.CountOfSmallerNumbersAfterSelf.js
+++ LeetCode/BinarySearch&DivideAndConquer/315.CountOfSmallerNumbersAfterSelf.js
@@ -66,3 +66,48 @@ var countSmaller = function (nums) {
   })
   return result.reverse()
 }
+
+// 解法三: 将原数据离散化, 记录之前的 index, val, 离散化之后对应的值
+// 倒序插入线段树, 以离散化后的数据作为数据的index, 将数据插入对应的 index 处, 插入过程中, 记录比它小的 index 数量, 即离散后比它小的值
+var countSmaller = function (nums) {
+  // 将 nums离散化成 [val, 离散值, index]
+  let bindNums = nums.map((num, index) => [num, 0, index])
+  bindNums.sort((a, b) => a[0] - b[0])
+  let discretization = []
+  for (let i = 0, j = -1; i < bindNums.length; i++) {
+    if (i === 0 || bindNums[i][0] !== bindNums[i - 1][0]) {
+      j++
+    }
+    discretization[i] = [bindNums[i][0], j, bindNums[i][2]]
+  }
+  // 离散后的值按 index 倒序排列
+  discretization.sort((a, b) => b[2] - a[2])
+  let result = new Array(discretization.length).fill(0)
+  let segementTree = new Array(discretization.length * 4).fill(0)
+  // 倒序后的离散值插入线段树中
+  for (let i = 0; i < discretization.length; i++) {
+    buildSegementTree(0, 0, discretization.length - 1, discretization[i][1], discretization[i][2])
+  }
+
+  return result
+
+  function buildSegementTree(treeIndex, left, right, updateIndex, index) {
+    if (left === right) {
+      segementTree[treeIndex] += 1
+      return
+    }
+    let leftTreeIndex = treeIndex * 2 + 1
+    let rightTreeIndex = treeIndex * 2 + 2
+    let mid = Math.floor((left + right) / 2)
+
+    if (updateIndex <= mid) {
+      buildSegementTree(leftTreeIndex, left, mid, updateIndex, index)
+    } else {
+      // 插入过程中, 记录前面出现过的比当前小的离散值的数量
+      result[index] += segementTree[leftTreeIndex]
+      buildSegementTree(rightTreeIndex, mid + 1, right, updateIndex, index)
+    }
+
+    segementTree[treeIndex] = segementTree[leftTreeIndex] + segementTree[rightTreeIndex]
+  }
+}
