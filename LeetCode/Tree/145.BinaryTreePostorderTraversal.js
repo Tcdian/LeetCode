1bb953091112b5603e1607860df38e1ea23ed1af
Tcdian <wenkai>
2018-08-29 10:30:50 +0800

    145. Binary Tree Postorder Traversal

--- LeetCode/Tree/145.BinaryTreePostorderTraversal.js
+++ LeetCode/Tree/145.BinaryTreePostorderTraversal.js
@@ -32,3 +32,30 @@ var postorderTraversal = function (root) {
   }
   return result.reverse()
 }
+
+// 方法二: 记录第几次访问当前节点
+// 将节点放入 stack 中时, 记录是第几次访问当前节点, 第一次访问时, 标记访问次数, 然后再次放入 stack 中, 接着查看他的右子树
+// 第二次访问时, 将 val 放入result 中
+var postorderTraversal = function (root) {
+  if (root === null) {
+    return []
+  }
+  let result = []
+  let stack = []
+  let patrol = root
+  while (patrol !== null || stack.length !== 0) {
+    while (patrol !== null) {
+      stack.push([1, patrol])
+      patrol = patrol.left
+    }
+    let pairs = stack.pop()
+    if (pairs[0] === 1) {
+      pairs[0] = 2
+      stack.push(pairs)
+      patrol = pairs[1].right
+    } else {
+      result.push(pairs[1].val)
+    }
+  }
+  return result
+}
